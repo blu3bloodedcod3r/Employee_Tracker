22aedf751933da12617d1580a167fce73c8f3650
Crystal Aguilar <crystal_0688@yahoo.com>
2022-11-08 23:14:58 -0800

    readme and add/update

--- config/connection.js
+++ config/connection.js
@@ -3,9 +3,9 @@ require('dotenv').config();
 
 const db = mysql.createConnection({
     host: 'localhost',
-    user: 'root',
-    password: '',
-    database: "employee_db",
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
 })
 
 module.exports = db;
--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const inquirer = require('inquirer');
 const db = require('./config/connection')
-require('dotenv').config();
 
 function init () {
 inquirer.prompt([
@@ -73,9 +72,6 @@ inquirer.prompt([
 .catch(err => console.log(err))
 }
 
-//WHEN I choose to view all departments
-//THEN I am presented with a formatted table showing department names and department ids
-
 function viewAllDepts() {
     const sql = 'SELECT * FROM department';
     //review activity 21/22
@@ -88,11 +84,8 @@ function viewAllDepts() {
         .catch(err => console.log(err));   
 }
 
-// WHEN I choose to view all roles
-// THEN I am presented with the job title, role id, the department that role belongs to, and the salary for that role
-
 function viewAllRoles() {
-    const sql = 'SELECT *, department.name FROM role Join department ON department.id = role.department.id';
+    const sql = 'SELECT *, department.name FROM role Join department ON department.id = role.department_id';
     db.promise()
         .query(sql)
         .then(([rows, _]) => {
@@ -101,17 +94,14 @@ function viewAllRoles() {
         })
         .catch(err => console.log(err));
 }
-
-//WHEN I choose to view all employees
-//THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
 
 function viewAllEmps() {
     const sql = `SELECT emp.id, emp.first_name, emp.last_name, title, salary,
-                CONCAT(mgr.first_name, ' ', mngr.last_name AS manager
+                CONCAT(mngr.first_name, ' ', mngr.last_name) AS manager
                 FROM employee emp
                 LEFT JOIN employee mngr ON mngr.id = emp.manager_id
-                LEFT JOIN role On emp.role_id = role.id
-                LEFT JOIN role.department_id = department.id`;
+                LEFT JOIN role ON emp.role_id = role.id
+                LEFT JOIN department ON role.department_id = department.id;`;
 
     db.promise()
         .query(sql)
@@ -122,11 +112,17 @@ function viewAllEmps() {
         .catch(err => console.log(err));
 }
 
-//WHEN I choose to add a department
-//THEN I am prompted to enter the name of the department and that department is added to the database
-
 function addDept() {
-    const sql = 'SELECT name FROM department'
+    inquirer.prompt ([
+        {
+            type: 'input',
+            name: 'add_department',
+            message: 'What is the name of the department would you like to add?',
+        }
+    ]).then(answer => {
+        console.log(answer.add_department)
+    })
+    const sql = 'INSERT name FROM department'
     db.promise()
     .query(sql)
     .then(([rows, _]) => {
@@ -135,12 +131,19 @@ function addDept() {
     })
     .catch(err => console.log(err));
 }
-
-// WHEN I choose to add a role
-// THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
 
 function addRole() {
-    const sql = 'SELECT role.title, role.salary FROM role LEFT JOIN department ON department.name'
+    inquirer.prompt ([
+        {
+            type: 'input',
+            name: 'add_role',
+            message: 'What is the role would you like to add?',
+        }
+    ]).then(answer => {
+        console.log(answer.add_role)
+    })
+
+    const sql = 'INSERT role.title, role.salary FROM role LEFT JOIN department ON department.name'
     db.promise()
     .query(sql)
     .then(([rows, _]) => {
@@ -149,19 +152,27 @@ function addRole() {
     })
     .catch(err => console.log(err));
 }
-
-// WHEN I choose to add an employee
-// THEN I am prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
 
 function addEmp() {
     inquirer.prompt ([
         {
-
+            type: 'input',
+            name: 'add_emp_first',
+            message: `What is the first_name of the employee would you like to add?`,
+        },
+        {
+            type: 'input',
+            name: 'add_emp_last',
+            message: `What is the last_name of the employee would you like to add?`,
         }
-    ])
-    const sql = `SELECT emp.id, emp.first_name, emp.last_name, role.title
+    ]).then(answer => {
+        console.log(answer.add_emp_first)
+        console.log(answer.add_emp_last)
+    })
+
+    const sql = `INSERT emp.id, emp.first_name, emp.last_name, role.title
     CONCAT(mgr.first_name, ' ', mngr.last_name AS manager
-    FROM employee emp
+    INTO employee emp
     LEFT JOIN employee mngr ON mngr.id = emp.manager_id
     LEFT JOIN role On emp.role_id = role.id
     LEFT JOIN role.department_id = department.id`
@@ -174,11 +185,8 @@ function addEmp() {
     .catch(err => console.log(err));
 }
 
-// WHEN I choose to update an employee role
-// THEN I am prompted to select an employee to update and their new role and this information is updated in the database 
-
 function updateEmpRole() {
-    const sql = 'SELECT emp.role_id, emp.id FROM role ON role.id = emp.role.id'
+    const sql = 'UPDATE emp.role_id, emp.id SETemployee WHERE role ON role.id = emp.role.id'
     db.promise()
     .query(sql)
     .then(([rows, _]) => {
@@ -187,6 +195,5 @@ function updateEmpRole() {
     })
     .catch(err => console.log(err));
 }
-
 
 init();
